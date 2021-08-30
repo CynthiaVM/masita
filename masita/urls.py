b5a0070d6deac1aa5330bcf0433045b738514a27
CynthiaVM <cynthiamouzo@hotmail.com>
2021-08-30 19:09:49 -0300

    algo

--- masita/urls.py
+++ masita/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ("", views.home, name="home"),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
 ]
